b6b7603e914cfbb4946d9648d791d9de9f0ae18e
RaphaelFontaine <raphael.fontaine62@outlook.fr>
2023-03-03 17:38:28 +0100

    logs file

--- start.sh
+++ start.sh
@@ -1,16 +1,57 @@
 #!/bin/bash
 
-echo "------ MY WALLET REPARTITION DIAGRAM ------"
+logs="/Users/raphaelfontaine/Documents/GIT/Binance/logs/"
+date=$(date +%Y-%m-%d)
 
-cd /Users/raphaelfontaine/Documents/GIT/Binance/src
-/Users/raphaelfontaine/Documents/GIT/Binance/venv_binance/bin/python3 wallet_repartition.py
+if [ "$#" -eq 0 ]; 
+    then
+        echo "------ MY WALLET REPARTITION DIAGRAM ------"
 
-echo "GO TO  /data/wallet_repartition TO SEE IT"
+        logs_path=$logs"wallet_repartition/"$date".log"
+
+        cd /Users/raphaelfontaine/Documents/GIT/Binance/src
+        /Users/raphaelfontaine/Documents/GIT/Binance/venv_binance/bin/python3 wallet_repartition.py >> $logs_path
+
+        echo "GO TO  /data/wallet_repartition TO SEE IT"
 
 
-echo "------ MY WALLET VALUE DIAGRAM ------"
+        echo "------ MY WALLET VALUE DIAGRAM ------"
 
-cd /Users/raphaelfontaine/Documents/GIT/Binance/src
-/Users/raphaelfontaine/Documents/GIT/Binance/venv_binance/bin/python3 wallet_value.py
+        logs_path=$logs"wallet_value/"$date".log"
 
-echo "GO TO  /data/wallet_value TO SEE IT"
+        cd /Users/raphaelfontaine/Documents/GIT/Binance/src
+        /Users/raphaelfontaine/Documents/GIT/Binance/venv_binance/bin/python3 wallet_value.py >> $logs_path
+
+        echo "GO TO  /data/wallet_value TO SEE IT"
+
+elif [ "$#" -eq 1 ]; 
+    then
+        if [ $1 == "v" ];
+            then 
+                echo "------ MY WALLET VALUE DIAGRAM ------"
+
+                logs_path=$logs"wallet_value/"$date".log"
+
+                cd /Users/raphaelfontaine/Documents/GIT/Binance/src
+                /Users/raphaelfontaine/Documents/GIT/Binance/venv_binance/bin/python3 wallet_value.py >> $logs_path
+
+                echo "GO TO  /data/wallet_value TO SEE IT"
+
+        elif [ $1 == "r" ];
+            then 
+                echo "------ MY WALLET REPARTITION DIAGRAM ------"
+
+                logs_path=$logs"wallet_repartition/"$date".log"
+                echo $logs_path
+
+                cd /Users/raphaelfontaine/Documents/GIT/Binance/src
+                /Users/raphaelfontaine/Documents/GIT/Binance/venv_binance/bin/python3 wallet_repartition.py >> $logs_path
+
+                echo "GO TO  /data/wallet_repartition TO SEE IT"
+        else
+            echo "Enter one of the following argument depending on what diagraph you want \n - r : repartition of yor wallet \n - v : value of your wallet "
+        fi
+
+else
+    echo "Enter zero or one argument. If you enter one arg, enter of the following argument depending on what diagraph you want \n - r : repartition of yor wallet \n - v : value of your wallet "
+fi
