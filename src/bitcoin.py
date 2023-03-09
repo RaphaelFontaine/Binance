4c6ced3eca30bafe4e7c79528031a3ade4217912
RaphaelFontaine <raphael.fontaine62@outlook.fr>
2023-03-09 23:47:47 +0100

    bitcoin values OK

--- src/bitcoin.py
+++ src/bitcoin.py
@@ -2,6 +2,7 @@ from prometheus_client import Histogram, start_http_server
 import requests
 import json
 import time
+from datetime import datetime, timedelta
 import sys
 
 
@@ -15,16 +16,19 @@ def bitcoin_value():
     # Boucle pour envoyer des données à Prometheus
     date = "2021-10-10"
     while date < time.strftime("%Y-%m-%d"):
-        print(date)
         # Récupérer la valeur du bitcoin pour la date actuelle
         response = requests.get(f"https://api.coindesk.com/v1/bpi/historical/close.json?start={date}&end={date}")
         data = json.loads(response.text)
         value = data['bpi'][date]
+        print(value)
         # Ajouter la valeur à l'histogramme
         bitcoin_values.observe(float(value))
         # Passer à la date suivante
-        date = time.strftime("%Y-%m-%d", time.gmtime(time.mktime(time.strptime(date, "%Y-%m-%d")) + 86400))
+        date = datetime.strptime(date, '%Y-%m-%d')
+        date = date + timedelta(days=1)
+        date = date.strftime('%Y-%m-%d')
         print(date)
+        # date = time.strftime("%Y-%m-%d", time.gmtime(time.mktime(time.strptime(date, "%Y-%m-%d")) + 86400))
     print(bitcoin_values)
     return bitcoin_values
 
