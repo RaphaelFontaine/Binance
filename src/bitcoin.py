9d9fdbb1843ad95210a96654758529586fd1fd6d
RaphaelFontaine <raphael.fontaine62@outlook.fr>
2023-03-10 23:53:22 +0100

    bitcoin saved graph

--- src/bitcoin.py
+++ src/bitcoin.py
@@ -5,6 +5,7 @@ import time
 from datetime import datetime, timedelta
 import sys
 import os
+import matplotlib.pyplot as plt
 
 data_file = "/Users/raphaelfontaine/Documents/GIT/Binance/data/JSON_DATA/bitcoin_values.json"
 
@@ -17,11 +18,8 @@ def bitcoin_value():
     if (taille != 0):
         with open(data_file, 'r') as f:
             bitcoin_data = json.loads(f.read())
-        print(bitcoin_data)
         old_dates = list(bitcoin_data.keys())
-        print(old_dates)
         last_date = old_dates[len(old_dates)-1]
-        print(last_date)
     else : 
         last_date = "2021-10-10"
         bitcoin_data = {}
@@ -30,15 +28,12 @@ def bitcoin_value():
         # Récupérer la valeur du bitcoin pour la date actuelle
         response = requests.get(f"https://api.coindesk.com/v1/bpi/historical/close.json?start={last_date}&end={last_date}")
         data = json.loads(response.text)
-        print(data)
         try:
             value = data['bpi'][last_date]
         except:
             last_key = list(bitcoin_data.keys())[-1]
-            print(last_key)
             value = bitcoin_data[last_key]
             
-        print(value)
         bitcoin_data[last_date] = value
         # Ajouter la valeur à l'histogramme
         bitcoin_values.observe(float(value))
@@ -46,21 +41,45 @@ def bitcoin_value():
         last_date = datetime.strptime(last_date, '%Y-%m-%d')
         last_date = last_date + timedelta(days=1)
         last_date = last_date.strftime('%Y-%m-%d')
-        print(last_date)
         
         # date = time.strftime("%Y-%m-%d", time.gmtime(time.mktime(time.strptime(date, "%Y-%m-%d")) + 86400))
     with open(data_file, 'w') as f:
         json.dump(bitcoin_data, f, indent=4, sort_keys=True)
 
-    print(bitcoin_data)
     return bitcoin_values
 
-def graph_bitcoin(bitcoin_values):
-    return 0
+def graph_bitcoin():
+    with open(data_file, 'r') as f:
+        bitcoin_data = json.loads(f.read())
+
+    dates = list(bitcoin_data.keys())
+    values = list(bitcoin_data.values())
+    
+    dates = [datetime.strptime(date, "%Y-%m-%d") for date in dates]
+
+    # Création de la figure et du graphe
+    fig, ax = plt.subplots()
+
+    # Tracé du graphe
+    ax.plot(dates, values)
+
+    # Configuration des étiquettes d'axe
+
+    # Affichage du graphe
+    plt.xlabel("Date")
+    plt.ylabel("Bitcoin value")
+    plt.title("Variation of bitcoin value siince October 2021")
+    plt.xticks(rotation=45, ha='right')
+    plt.tight_layout()
+    # plt.show()
+
+    aujourd_hui = datetime.now().strftime('%Y-%m-%d')
+    plt.savefig('../data/bitcoin/'+aujourd_hui+'.png')
 
 
 def main():
     bitcoin_value()
+    graph_bitcoin()
 
     # Démarrer le serveur HTTP de Prometheus
     start_http_server(8000)
